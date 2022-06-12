d1f221d00244783387fe263766d2693a830337f7
Dillon Drenzek <dillon@drenzek.com>
2022-06-11 22:14:36 -0700

    chess state to chess-types.ts

--- src/lib/chess-types.ts
+++ src/lib/chess-types.ts
@@ -31,3 +31,9 @@ export type BoardFile = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H";
 export type BoardRank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 
 export type SquareColor = "light" | "dark";
+
+export interface ChessState {
+  pieces: (Piece | null)[];
+
+  turn: PieceColor;
+}
