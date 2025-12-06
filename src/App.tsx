0cfb689480cd1571a556e6be95654870f8739f0b
Dillon Drenzek <dillon@drenzek.com>
2025-12-06 00:29:18 -0800

    introduce debug mode

--- src/App.tsx
+++ src/App.tsx
@@ -17,15 +17,20 @@ import {
   Grid,
   Group,
   MantineProvider,
+  Paper,
   Stack,
+  Switch,
+  Title,
 } from "@mantine/core";
 import { SquareColor } from "./lib/chess-types";
+import { useAppContext } from "./lib/app-context";
 
 const BOARD_HEIGHT = 500;
 const BOARD_WIDTH = 500;
 
 function App() {
   // const fenString = new URL(window.location.href).searchParams.get("fen");
+  const { debugMode, setDebugMode } = useAppContext();
   const {
     pieces,
     movePieceToSquare,
@@ -56,53 +61,62 @@ function App() {
         }}
       >
         <AppShell.Header>
-          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-
-          <div>Chess Client</div>
+          <Center h="100%" w="100%">
+            <Group px="md" justify="space-between" w="100%">
+              <Title order={2} p={0}>
+                Chess Client
+              </Title>
+              <Switch
+                label="Debug mode"
+                checked={debugMode}
+                onChange={(event) => setDebugMode(event.target.checked)}
+              />
+            </Group>
+          </Center>
         </AppShell.Header>
 
-        <AppShell.Aside>
-          <Group p="md">
-            <Stack>
-              <Code>
-                <pre>{fen}</pre>
-              </Code>
+        {debugMode && (
+          <AppShell.Aside>
+            <Group p="md">
+              <Stack>
+                <Code>
+                  <pre>{fen}</pre>
+                </Code>
 
-              <Code>
-                <pre>{ascii}</pre>
-              </Code>
+                <Code>
+                  <pre>{ascii}</pre>
+                </Code>
 
-              <Group>
-                {turn === "b" ? (
-                  <Badge color="dark">Black</Badge>
-                ) : (
-                  <Badge color="gray">White</Badge>
-                )}
-                {isCheckmate ? <Badge color="green">Checkmate</Badge> : null}
-                {!isCheckmate && isCheck ? (
-                  <Badge color="yellow">Check</Badge>
-                ) : null}
-                {!isCheckmate && isGameOver ? (
-                  <Badge color="gray">Game Over</Badge>
-                ) : null}
-              </Group>
+                <Group>
+                  {turn === "b" ? (
+                    <Badge color="dark">Black</Badge>
+                  ) : (
+                    <Badge color="gray">White</Badge>
+                  )}
+                  {isCheckmate ? <Badge color="green">Checkmate</Badge> : null}
+                  {!isCheckmate && isCheck ? (
+                    <Badge color="yellow">Check</Badge>
+                  ) : null}
+                  {!isCheckmate && isGameOver ? (
+                    <Badge color="gray">Game Over</Badge>
+                  ) : null}
+                </Group>
 
-              <Group>
-                <Button onClick={reset}>Reset</Button>
-              </Group>
-            </Stack>
-          </Group>
-        </AppShell.Aside>
+                <Group>
+                  <Button onClick={reset}>Reset</Button>
+                </Group>
+              </Stack>
+            </Group>
+          </AppShell.Aside>
+        )}
 
         <AppShell.Main>
-          <Stack>
-            <BoardStage
-              height={BOARD_HEIGHT}
-              width={BOARD_WIDTH}
-              pieces={pieces}
-              movePieceToSquare={movePieceToSquare}
-            />
-          </Stack>
+          <BoardStage
+            height={BOARD_HEIGHT}
+            width={BOARD_WIDTH}
+            pieces={pieces}
+            movePieceToSquare={movePieceToSquare}
+          />
         </AppShell.Main>
       </AppShell>
 
--- src/lib/app-context.tsx
+++ src/lib/app-context.tsx
@@ -1,16 +1,43 @@
-import { useContext, createContext, PropsWithChildren } from "react";
+import React, {
+  useContext,
+  createContext,
+  PropsWithChildren,
+  useState,
+  useMemo,
+  useCallback,
+} from "react";
 
 const defaultAppContext = {
   debugMode: false,
+  setDebugMode: (a: boolean) => {
+    return;
+  },
 };
 
-const appContext = createContext(defaultAppContext);
+const appContext = createContext<
+  typeof defaultAppContext & {
+    setDebugMode: (a: boolean) => void;
+  }
+>(defaultAppContext);
 
 export function AppContextProvider(props: PropsWithChildren) {
+  const [debugMode, setDebugMode] = useState(defaultAppContext.debugMode);
+
+  const handleSetDebugMode = useCallback(
+    (isDebugMode: boolean) => setDebugMode(isDebugMode),
+    []
+  );
+
+  const value = useMemo(() => {
+    return {
+      ...defaultAppContext,
+      debugMode,
+      setDebugMode: handleSetDebugMode,
+    };
+  }, [debugMode, handleSetDebugMode]);
+
   return (
-    <appContext.Provider value={defaultAppContext}>
-      {props.children}
-    </appContext.Provider>
+    <appContext.Provider value={value}>{props.children}</appContext.Provider>
   );
 }
 
